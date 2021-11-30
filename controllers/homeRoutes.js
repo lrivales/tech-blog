f1d5d10d411c313a74f2fc14afb252e3eec3d9bf
Lawrence Rivales <lrivales@gmail.com>
2021-11-30 13:20:00 -0500

    added homepage view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,40 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const { QueryTypes, json } = require('sequelize');
 
 router.get('/', async (req, res) => {
-    const posts = await sequelize.query(
-        "SELECT `post`.`id`, `post`.`title`, `post`.`post_text`, `post`.`user_id`, `post`.`created_at` AS `createdAt`, `post`.`updated_at` AS `updatedAt`, `post`.`user_id` AS `userId`, `user`.`username` AS `author`, `comments`.`comment_text` AS `post_comment_text`, `comments`.`created_at` AS `comment_date`, `comments->user`.`username` AS `commenter` FROM `post` AS `post` LEFT OUTER JOIN `user` AS `user` ON `post`.`user_id` = `user`.`id` LEFT OUTER JOIN `comment` AS `comments` ON `post`.`id` = `comments`.`post_id` LEFT OUTER JOIN `user` AS `comments->user` ON `comments`.`user_id` = `comments->user`.`id` ORDER BY `post`.`updated_at` DESC",
-        { type: QueryTypes.SELECT });
-
-    console.log(posts);
-    res.render('homepage', {posts});
+    Post.findAll(
+        {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['comment_text', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        }
+    )
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log(posts);
+        console.log(posts[0].comments);
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
-
-
 
 router.get('/login', async (req, res) => {
     
